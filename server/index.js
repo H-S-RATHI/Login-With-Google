a30a893f9b091145f09a3757361d544502d4a98f
H-S-RATHI <goluu.000111@gmail.com>
2024-01-15 20:49:08 +0530

    update

--- server/index.js
+++ server/index.js
@@ -39,8 +39,7 @@ passport.use(
     {
       clientID: clientid,
       clientSecret: clientsecret,
-      callbackURL:
-        "https://login-with-google-server.vercel.app/auth/google/callback",
+      callbackURL: "https://login-with-google-server.vercel.app/auth/google",
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
