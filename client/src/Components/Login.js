67b0407957636395291c8a8203824c77eac6a9d2
H-S-RATHI <goluu.000111@gmail.com>
2024-01-15 20:24:36 +0530

    updated code

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -4,7 +4,7 @@ import "./login.css";
 const Login = () => {
   const loginwithgoogle = () => {
     window.open(
-      "https://login-with-google-server.vercel.app/auth/google",
+      "https://login-with-google-server.vercel.app/auth/google/callback",
       "_self"
     );
   };
